33a9a8aaff876e07fa161c2ab1b46bc3071094dc
toshiomi <19881005mnmtsom@gmail.com>
2019-12-05 18:48:39 +0900

    詳細ページ確認機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
   resources :list, only: [:new, :create, :edit, :update, :destroy] do
-    resources :card, only: [:new, :create]
+    resources :card, only: [:new, :create, :show]
   end
 end
