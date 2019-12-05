48dfb4efbca996476642872f286c71db9f5d80b1
toshiomi <19881005mnmtsom@gmail.com>
2019-12-05 15:49:27 +0900

    編集機能の追加

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -13,6 +13,19 @@ class ListController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find_by(id: params[:id])
+  end
+
+  def update
+    @list = List.find_by(id: params[:id])
+    if @list.update_attributes(list_params)
+      redirect_to root_path
+    else
+      render 
+    end
+  end
+
   private
   def list_params
     params.require(:list).permit(:title).merge(user: current_user)
