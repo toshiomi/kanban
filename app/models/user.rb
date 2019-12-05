2d13e99dda54a14acd0af9b79f42a795c6eaf92b
toshiomi <19881005mnmtsom@gmail.com>
2019-12-05 12:34:45 +0900

    ヘッダーにサインインしたユーザーを表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 20 }
 end
