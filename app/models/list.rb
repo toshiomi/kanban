037a10aea8b501480c499562bff17be0343de0e4
toshiomi <19881005mnmtsom@gmail.com>
2019-12-05 16:32:45 +0900

    リスト削除時に紐づくカードも削除されるよう変更

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
-  has_many :cards
+  #リストを削除した時にそれに関するカードも削除する
+  has_many :cards, dependent: :destroy
   validates :title, length: { in: 1..255 }
 end
