bd73ed6bb3624f0c881cb778e932b7a62c3e03e1
toshiomi <19881005mnmtsom@gmail.com>
2019-12-05 17:48:47 +0900

    カード作成機能の実装

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,2 +1,20 @@
 class CardController < ApplicationController
+  def new
+    @card = Card.new
+    @list = List.find_by(id: params[:list_id])
+  end
+
+  def create
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to root_path
+    else
+      render new_list_card_path
+    end
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:title, :memo, :list_id)
+  end
 end
